9e820fe0a0d83f0cf7be12caacb00cebd96b44e6
unknown <aliahmadfahs17@gmail.com>
2025-08-08 19:59:01 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,99 @@
+import json
+import time
+import asyncio
+from rnet import Impersonate,BlockingClient
+import pandas as pd
+import gspread
+from gspread_dataframe import set_with_dataframe
+from oauth2client.service_account import ServiceAccountCredentials
+from camoufox.sync_api import Camoufox
+userNickname=[]
+title=[]
+formattedDate=[]
+text=[]
+isRecommended=[]
+rating=[]
+badges=[]
+positiveFeedbackCount=[]
+negativeFeedbackCount=[]
+
+start =time.perf_counter()
+def create_client():
+    return BlockingClient(impersonate=Impersonate.Firefox136)
+
+def get_reviews(client,url):
+    
+    return  client.get(url)
+
+
+def save_data_into_csv(userNickname,title,formattedDate,text,isRecommended,rating,badges,positiveFeedbackCount,negativeFeedbackCount):
+
+
+    # Create DataFrame from dict where keys become column names
+    df = pd.DataFrame({
+        'userNickname': userNickname,
+        'title': title,
+        'formattedDate': formattedDate,
+        'text': text,
+        'isRecommended': isRecommended,
+        'rating': rating,
+        'badges': badges,
+        'positiveFeedbackCount': positiveFeedbackCount,
+        'negativeFeedbackCount':negativeFeedbackCount,
+
+    })
+
+
+    # Setup Google Sheets credentials
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open("adidas_review").worksheet("Sheet1")
+
+    # Write DataFrame to shee   t
+    sheet.clear()  # optional: clear existing data
+    set_with_dataframe(sheet, df)
+
+
+    # Save to CSV
+
+    df.to_csv('output.csv', index=True)
+
+def main(model_number):    
+    
+    client=create_client()
+    initial_page= client.get(f'https://www.adidas.com/api/models/{model_number}/reviews?bazaarVoiceLocale=en_US&feature&includeLocales=en%2A&limit=5&offset=22&sort=newest')
+    review_nb=json.loads( initial_page.text())['totalResults']
+    for i in range(0,review_nb,10):
+        print(i)
+        url=f'https://www.adidas.com/api/models/{model_number}/reviews?bazaarVoiceLocale=en_US&feature&includeLocales=en%2A&limit=10&offset={i}&sort=newest'
+        reviews_resp= get_reviews(client,url)
+        reviews_data=json.loads( reviews_resp.text())['reviews']
+        for x in reviews_data:   
+            userNickname.append(str(x['userNickname']).strip().replace('\n', ' ').replace('\r', ' '))
+            title.append(str(x['title']).strip().replace('\n', ' ').replace('\r', ' '))
+            formattedDate.append(str(x['formattedDate']).strip().replace('\n', ' ').replace('\r', ' '))
+            text.append(str(x['text']).strip().replace('\n', ' ').replace('\r', ' '))
+            isRecommended.append(str(x['isRecommended']).strip().replace('\n', ' ').replace('\r', ' '))
+            rating.append(str(x['rating']).strip().replace('\n', ' ').replace('\r', ' '))
+            badges.append(str(x['badges']).strip().replace('\n', ' ').replace('\r', ' '))
+            positiveFeedbackCount.append(str(x['positiveFeedbackCount']).strip().replace('\n', ' ').replace('\r', ' '))
+            negativeFeedbackCount.append(str(x['negativeFeedbackCount']).strip().replace('\n', ' ').replace('\r', ' '))
+    save_data_into_csv(userNickname,title,formattedDate,text,isRecommended,rating,badges,positiveFeedbackCount,negativeFeedbackCount)
+
+if __name__=="__main__":
+    with Camoufox(headless=True) as browser:
+        product_id='JI0183'
+        page = browser.new_page()
+        page.goto(f"https://www.adidas.com/us/samba-og-shoes/{product_id}.html")
+        
+        model_number=json.loads(page.locator('div#consent_blackbar+script+script').inner_text().replace("window.REACT_QUERY_DATA = ",''))['queries'][0]['state']['data']['model_number']
+    print(f'done with camoufox with model_number : {model_number}')
+
+    main(model_number)
+end = time.perf_counter()
+print(f"Script finished in {end - start:.2f} seconds")
+
+
+# https://adidas-review-872349921472.africa-south1.run.app
